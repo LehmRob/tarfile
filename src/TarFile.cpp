7b085917de42f3e5ab2f9bb71c08e4436578a2db
Robert Lehmann <lehmrob@gmail.com>
2021-08-29 16:01:11 +0200

    TarFile: Cleanup

--- src/TarFile.cpp
+++ src/TarFile.cpp
@@ -2,23 +2,15 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include "TarFile.hpp"
+#include "TarFile.h"
 
 #include <array>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
 
-#include "memcpy.hpp"
-
 namespace tarfile {
 
-TarFile TarFile::constructFromFile(const char *filename) {
-    std::array<char, 512> headerData = {'a', 'b', 'c'};
-
-    auto h = Header::construct(headerData);
-
-    return TarFile(h);
-}
+TarFile TarFile::constructFromFile(const char *filename) {}
 
 } // namespace tarfile
