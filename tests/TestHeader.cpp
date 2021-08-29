e76a37817d5d0e6bb6e1b6e0f60361aeca209fd0
Robert Lehmann <lehmrob@gmail.com>
2021-08-29 16:02:13 +0200

    tests/TestHeader: Adding first test assertions

--- tests/TestHeader.cpp
+++ tests/TestHeader.cpp
@@ -8,6 +8,18 @@
 
 #include <array>
 
+#include "../src/Header.h"
+
 #include "hello.tar.h"
 
-TEST_CASE("Test if testframework works", "[HEADER]") { REQUIRE(1 == 1); }
+using Catch::Matchers::Contains;
+
+TEST_CASE("Test if testframework works", "[HEADER]") {
+    auto header = tarfile::Header::construct(hello_data, 512);
+
+    REQUIRE_THAT(header.name(), Contains("hello.txt"));
+    REQUIRE(header.mode() == 0644);
+    REQUIRE(header.uid() == 1750);
+    REQUIRE(header.gid() == 1750);
+    REQUIRE(header.size() == 15);
+}
