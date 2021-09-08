123889e977c2bcc3cff722ff374954ad05bfdeb8
Robert Lehmann <lehmrob@gmail.com>
2021-09-08 19:18:03 +0200

    simple: Void unused parameters

--- example/simple.cpp
+++ example/simple.cpp
@@ -4,7 +4,10 @@
 #include "../src/TarFile.h"
 
 int main(int ac, char *av[]) {
-    auto tf = tarfile::TarFile::constructFromFile("test");
+    (void)ac;
+    (void)av;
+
+    // auto tf = tarfile::TarFile::constructFromFile("test");
 
     return 0;
 }
